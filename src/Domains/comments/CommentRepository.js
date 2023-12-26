d08f07fd78396bc1d5f2f1e3c8792131d54a7acb
Anindya Sekar Paramitha <anindyasekarp@gmail.com>
2023-12-26 17:49:01 +0700

    Modify comment repository

--- src/Domains/comments/CommentRepository.js
+++ src/Domains/comments/CommentRepository.js
@@ -6,6 +6,9 @@ class CommentRepository {
   async verifyCommentOwner(commentId, userId) {
     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+  async getCommentById(commentId) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
   async getCommentsByThreadId(threadId) {
     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
